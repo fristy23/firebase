6cb8e23870a5c3ed9ea12dd9c5335c8027008268
sakshi sharma <sakshish63@gmail.com>
2023-05-20 13:55:32 +0530

    done

---
+++ src/app/adminPages/addemployee/addemployee.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addemployee',
+  templateUrl: './addemployee.component.html',
+  styleUrls: ['./addemployee.component.scss'],
+})
+export class AddemployeeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
