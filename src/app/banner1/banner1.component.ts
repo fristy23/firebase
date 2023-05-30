2b5b3188150a6ed91ed559cd43d03099ebbd699c
sakshi sharma <sakshish63@gmail.com>
2023-05-30 13:08:41 +0530

    done

--- src/app/banner1/banner1.component.ts
+++ src/app/banner1/banner1.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { doc, getDoc,getDocs,Timestamp,collection, getFirestore,deleteDoc, updateDoc, deleteField  } from "firebase/firestore";
 
+const db = getFirestore();
 @Component({
   selector: 'app-banner1',
   templateUrl: './banner1.component.html',
@@ -7,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Banner1Component  implements OnInit {
 
+  collectbannerdata:any=[this.alldocument()];
   constructor() { }
 
   ngOnInit() {}
-
+  async  alldocument()
+  {
+   console.log("alldocument");
+   var list:any=[];
+ const querySnapshot = await getDocs(collection(db, "banner"));
+ querySnapshot.forEach((doc) => {
+   // doc.data() is never undefined for query doc snapshots
+   //console.log(doc.id, " => ", doc.data());
+   list.push(doc.data())
+ });
+ this.collectbannerdata = list;
+ console.log(this.collectbannerdata)
+  }
+ 
 }
